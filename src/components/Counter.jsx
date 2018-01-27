8adae3c5da5ebfb73024d78986e4e0148202414d
navono <navono007@gmail.com>
2018-01-27 11:09:58 +0800

    组件中删除本地化的操作

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,26 +3,9 @@ import React from 'react';
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      counter: 0,
-    }
-  }
-
-  handleIncreament = (e) => {
-    this.setState((prevState, porps) => ({
-      counter: prevState.counter + 1
-    }));
-  }
-
-  handleDecreament = (e) => {
-    this.setState((prevState, porps) => ({
-      counter: prevState.counter - 1
-    }));
   }
 
   render() {
-    console.log(this.props);
     return (
       <div>
         <p>Hello, World! I'm counting...</p>
